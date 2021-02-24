df3a0674cd039b8549385bb3a92a9512ebffdcf1
Amir Afshar <mr.afshar0@gmail.com>
2021-02-25 02:38:03 +0330

    Add an if else statement for different js functions in mobile/desktop use

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const active = document.querySelector(".shareactive");
 const details = document.querySelector(".details");
 const button = document.querySelector(".share-btn");
 const btnactive = document.querySelector(".share-btn-active");
+
+if(window.innerWidth<=480){
 button.addEventListener("click", ()=>{
     if(details.style.display = "flex"){
         details.style.display = "none";
@@ -14,3 +16,6 @@ btnactive.addEventListener("click", ()=>{
         active.style.display = "none";
     }
 });
+}else{
+    console.log("Desktop Design is in queue");
+}
