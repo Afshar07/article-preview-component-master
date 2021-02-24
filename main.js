78213542079bd7193609219b25ba48d22b5ab427
Amir Afshar <mr.afshar0@gmail.com>
2021-02-25 00:10:55 +0330

    Add functions to make the share btn dynamic

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const active = document.querySelector(".shareactive");
 const details = document.querySelector(".details");
 const button = document.querySelector(".share-btn");
+const btnactive = document.querySelector(".share-btn-active");
+button.addEventListener("click", ()=>{
+    if(details.style.display = "flex"){
+        details.style.display = "none";
+        active.style.display = "flex";
+    }
+});
+btnactive.addEventListener("click", ()=>{
+    if(details.style.display = "none"){
+        details.style.display = "flex";
+        active.style.display = "none";
+    }
+});
