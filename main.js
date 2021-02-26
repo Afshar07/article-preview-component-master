327408b3f8b65381bd4b6e5b53a350cbabf033a4
Amir Afshar <mr.afshar0@gmail.com>
2021-02-27 02:01:36 +0330

    Add a listener for desktop share btn

--- main.js
+++ main.js
@@ -2,20 +2,30 @@ const active = document.querySelector(".shareactive");
 const details = document.querySelector(".details");
 const button = document.querySelector(".share-btn");
 const btnactive = document.querySelector(".share-btn-active");
-
-if(window.innerWidth<=480){
-button.addEventListener("click", ()=>{
-    if(details.style.display = "flex"){
-        details.style.display = "none";
-        active.style.display = "flex";
+const deskactive = document.querySelector(".shareactive-desk");
+const btnbackground = document.querySelector(".share");
+if (window.innerWidth <= 480) {
+  button.addEventListener("click", () => {
+    if ((details.style.display = "flex")) {
+      details.style.display = "none";
+      active.style.display = "flex";
     }
-});
-btnactive.addEventListener("click", ()=>{
-    if(details.style.display = "none"){
-        details.style.display = "flex";
-        active.style.display = "none";
+  });
+  btnactive.addEventListener("click", () => {
+    if ((details.style.display = "none")) {
+      details.style.display = "flex";
+      active.style.display = "none";
     }
-});
-}else{
-    console.log("Desktop Design is in queue");
-}
+  });
+} else {
+// Desktop
+btnbackground.addEventListener("click", ()=>{
+    deskactive.classList.toggle("active");
+    btnbackground.classList.toggle("btn-active");
+    if(btnbackground.classList.contains("btn-active")){
+    button.src = "images/icon-share-active.svg";
+    }else{
+        button.src = "images/icon-share.svg";
+    }
+})
+};
